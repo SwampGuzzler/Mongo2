d70cd46c1f0c15feb521870c588dc2c711e5b3ea
Lucas Cotner <lc07@uw.edu>
2014-06-19 17:50:18 -0700

    Returning Mongo documents now...

--- model/hikes.js
+++ model/hikes.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 exports.hikelist = function hikelist(hname,callback){
  var Hike = mongoose.model( 'Hike' );
- Hike.find({type:hname}, function (err, hikes) {
+ Hike.find({'type':hname}, function (err, hikes) {
   if(err){
    console.log(err);
   }else{
