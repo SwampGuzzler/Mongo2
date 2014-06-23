5c5fb6e323cc0afe33ab0849ef2f624c967c726e
Lucas Cotner <lc07@uw.edu>
2014-06-23 14:40:47 -0700

    Added map but not Leaflet and JQuery source we'll need to render the map because of size issues

--- model/db.js
+++ model/db.js
@@ -4,7 +4,6 @@ var hikeSchema = new mongoose.Schema({
 	type: String,
 	geometry: {
     type: mongoose.Schema.Types.Mixed,
-		//coordinates: mongoose.Schema.Types.Mixed
     coordinates: { type: mongoose.Schema.Types.Mixed, index: true}
 	},
 	properties: {
@@ -29,75 +28,7 @@ mongoose.connect( 'mongodb://localhost/test');
 mongoose.connect( 'mongodb://localhost/test', function (err, db) {
   if(err) return console.dir(err)
   //var collection = db.collection('hikes6');
-
   hikes6.collection.ensureIndex( { geometry : "2dsphere" }, function(err, result) {
     if(err) return console.dir(err);
   });
 });*/
-
-
-/*
-Db.connect("mongodb://localhost:27017/geodb", function(err, db) {
-  if(err) return console.dir(err)
-  var collection = db.collection('places');
-
-  collection.ensureIndex({loc: "2d"}, {min: -500, max: 500, w:1}, function(err, result) {
-    if(err) return console.dir(err);
-
-    collection.insert(document, {w:1}, function(err, result) {
-      if(err) return console.dir(err)
-    });
-  });
-}); */
-
-// Replacing Mongo Code w/ this
-//var mongoose = require('mongoose'),
-//db = mongoose.createConnection('localhost', 'test');
-//db.on('error', console.error.bind(console, 'connection error:'));
-//var Hike = mongoose.model('Hike', hikeSchema);
-
-/*
-Hike.find({'GroupName':hname}, function (err, hikes) {
- if(err){
-  onErr(err,callback);
- }else{
-  mongoose.connection.close();
-  console.log(hikes);
-  callback("",hikes);
- }
-});// end Team.find*/
-
-
-// Start of 'Mongo w/o Mongoose' Code
-/*
-exports.teamlist = function(gname,callback){
- db.open(function(err, db) {
-  if(!err) {
-   db.collection('teams', function(err, collection) {
-    if(!err){
-     collection.find({'GroupName':gname}).toArray(function(err, docs) {
-      if(!err){
-       db.close();
-       var intCount = docs.length;
-       if(intCount > 0){
-        var strJson = "";
-        for(var i=0; i<intCount;){
-         strJson += '{"country":"' + docs[i].country + '"}'
-         i=i+1;
-         if(i<intCount){strJson+=',';}
-        }
-        strJson = '{"GroupName":"'+gname+'","count":'+intCount+',"teams":[' + strJson + "]}";
-        console.log(strJson);;
-        callback("",JSON.parse(strJson));
-       }
-      }
-      else{onErr(err,callback);}
-     });//end collection.find
-    }
-    else{onErr(err,callback);}
-   });//end db.collection
-  }
-  else{onErr(err,callback);}
- });// end db.open
-};
-*/
--- model/map.js
+++ model/map.js
@@ -1,6 +1,18 @@
 var mongoose = require('mongoose');
 var assert = require('assert');
 
-exports.mapLogic = function mapLogic(hname,callback){
-	
+exports.mapLogic = function mapLogic(map){
+	var map = L.map('mapDiv').setView([49, -120], 7);
+
+    L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+        id: 'examples.map-20v6611k'
+    }).addTo(mapDiv);
+    console.log("Are we even accessing me????");
+    console.log(map);
+
+    $('#mapDiv').html(map);
+
 }// end exports.hikelist
+
+
